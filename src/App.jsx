63426900448f454f1e49cd9286cb04f57ff5547f
sinskiy <sinskiy@proton.me>
2024-08-10 17:50:34 +0300

    feat: show layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import { Home, Settings } from "lucide-react";
 import "./App.css";
+import { useState } from "react";
 
+// App
+// -> provide settings context
+// -> show start screen (play button, choose difficulty)
 function App() {
-  return <></>;
+  const [route, setRoute] = useState("home");
+
+  function handleHomeClick() {
+    setRoute("home");
+  }
+  return (
+    <>
+      <header className="header">
+        <button aria-label="home" onClick={handleHomeClick}>
+          <h1>memory card</h1>
+        </button>
+        <nav>
+          <button aria-label="home" onClick={handleHomeClick}>
+            <Home />
+          </button>
+          <button aria-label="settings">
+            <Settings />
+          </button>
+        </nav>
+      </header>
+      <main></main>
+    </>
+  );
 }
 
 export default App;
