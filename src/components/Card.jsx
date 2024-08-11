19a67614066c66b8f5ea27f18e8c2b70cbf29215
sinskiy <sinskiy@proton.me>
2024-08-11 10:53:29 +0300

    refactor: improve game structure

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,15 @@
 import { BASE_API_URL } from "../../lib/const";
+import "../styles/Card.css";
 
-export default function Card({ title, code }) {
+export default function Card({ handleCountryClick, title, code }) {
   return (
     <li className="card">
-      <figure>
-        <img src={`${BASE_API_URL}${code}.svg`} alt={title} />
-        <figcaption>{title}</figcaption>
-      </figure>
+      <button onClick={() => handleCountryClick(code)}>
+        <figure>
+          <img src={`${BASE_API_URL}${code}.svg`} alt={title} />
+          <figcaption>{title}</figcaption>
+        </figure>
+      </button>
     </li>
   );
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,15 +2,51 @@ import Card from "./Card";
 import "../styles/Game.css";
 import { DIFFICULTIES } from "../../lib/const";
 import { countries } from "../../lib/api";
+import { useState } from "react";
+import { random, shuffle } from "../../lib/helpers";
 
 export default function Game({ selectedDifficultyIndex }) {
-  const cards = [];
+  const { total, atTime } = DIFFICULTIES[selectedDifficultyIndex];
+  const [countries, setCountries] = useState(() => getBaseCountries(total));
+  const [clickedCountries, setClickedCountries] = useState([]);
 
-  const cardsAmount = DIFFICULTIES[selectedDifficultyIndex].atTime;
-  for (let i = 0; i < cardsAmount; i++) {
-    const [randomCode, randomTitle] =
-      countries[Math.floor(Math.random() * countries.length)];
-    cards.push(<Card key={i} title={randomTitle} code={randomCode} />);
+  function handleCountryClick(code) {
+    if (clickedCountries.includes(code)) {
+      console.log("you lost");
+    } else {
+      const newClickedCountries = [...clickedCountries, code];
+      setClickedCountries(newClickedCountries);
+
+      shuffleCountries();
+    }
   }
-  return <ul className="cards">{cards}</ul>;
+  function shuffleCountries() {
+    const shuffledCountries = shuffle(countries);
+    setCountries(shuffledCountries);
+  }
+
+  return (
+    <ul className="cards">
+      {countries.slice(0, atTime).map(([code, title]) => (
+        <Card
+          key={code}
+          handleCountryClick={handleCountryClick}
+          code={code}
+          title={title}
+        />
+      ))}
+    </ul>
+  );
+}
+
+function getBaseCountries(total) {
+  const baseCountries = [];
+  for (let i = 0; i < total; i++) {
+    baseCountries.push(getRandomCountry());
+  }
+  return baseCountries;
+}
+
+function getRandomCountry() {
+  return countries[random(countries.length)];
 }
