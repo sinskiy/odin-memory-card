322d30ffcfc97f1c4d7597f106054dfc1984fdbe
sinskiy <sinskiy@proton.me>
2024-08-11 10:16:18 +0300

    feat: load images on game start

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,10 @@
-export default function Card({ title }) {
+import { BASE_API_URL } from "../../lib/const";
+
+export default function Card({ title, code }) {
   return (
     <li className="card">
       <figure>
+        <img src={`${BASE_API_URL}${code}.svg`} alt={title} />
         <figcaption>{title}</figcaption>
       </figure>
     </li>
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,14 +1,16 @@
 import Card from "./Card";
 import "../styles/Game.css";
 import { DIFFICULTIES } from "../../lib/const";
+import { countries } from "../../lib/api";
 
 export default function Game({ selectedDifficultyIndex }) {
   const cards = [];
 
   const cardsAmount = DIFFICULTIES[selectedDifficultyIndex].atTime;
-  console.log(cardsAmount);
   for (let i = 0; i < cardsAmount; i++) {
-    cards.push(<Card key={i} title={i} />);
+    const [randomCode, randomTitle] =
+      countries[Math.floor(Math.random() * countries.length)];
+    cards.push(<Card key={i} title={randomTitle} code={randomCode} />);
   }
   return <ul className="cards">{cards}</ul>;
 }
